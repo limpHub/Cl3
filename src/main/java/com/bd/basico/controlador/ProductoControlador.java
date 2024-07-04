2340d25060b9fb4d8774d214a36e34db7b12c6ca
Alexander <alexander@example.com>
2024-07-04 04:53:03 -0500

    cuarto commit: proyecto terminado y probado Abanto Garcia

--- src/main/java/com/bd/basico/controlador/ProductoControlador.java
+++ src/main/java/com/bd/basico/controlador/ProductoControlador.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.bd.basico.modelo.*;
 import com.bd.basico.servicio.IProductoServicio;
-import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/Vistas")
